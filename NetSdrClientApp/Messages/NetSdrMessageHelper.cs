88ad774385d56dc55dace37e3047e3dd403c50d6
Nolfurh <97357330+Nolfurh@users.noreply.github.com>
2026-05-12 11:39:36 +0300

    Fix architecture violation dependency

--- NetSdrClientApp/Messages/NetSdrMessageHelper.cs
+++ NetSdrClientApp/Messages/NetSdrMessageHelper.cs
@@ -50,7 +50,6 @@ namespace NetSdrClientApp.Messages
 
         private static byte[] GetMessage(MsgTypes type, ControlItemCodes itemCode, byte[] parameters)
         {
-            var tcpClientWrapper = new NetSdrClientApp.Networking.TcpClientWrapper("For architecture test", 0);
             var itemCodeBytes = Array.Empty<byte>();
             if (itemCode != ControlItemCodes.None)
             {
